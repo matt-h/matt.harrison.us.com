b0371dca6ae96c59ed4593a7ac6c2b253cd4ad4d
Matt Harrison <matt@harrison.us.com>
2022-08-08 22:37:03 -0400

    setup config

--- config.php
+++ config.php
@@ -5,14 +5,14 @@ use Illuminate\Support\Str;
 return [
     'baseUrl' => '',
     'production' => false,
-    'siteName' => 'Blog Starter Template',
-    'siteDescription' => 'Generate an elegant blog with Jigsaw',
-    'siteAuthor' => 'Author Name',
+    'siteName' => 'Matt',
+    'siteDescription' => 'Developer and Open Source enthusiast',
+    'siteAuthor' => 'Matt',
 
     // collections
     'collections' => [
         'posts' => [
-            'author' => 'Author Name', // Default author, if not provided in a post
+            'author' => 'Matt', // Default author, if not provided in a post
             'sort' => '-date',
             'path' => 'blog/{filename}',
         ],
